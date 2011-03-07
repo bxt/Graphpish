6215c1936c640a0ec72927ccfff068849136f8a2
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-08 00:38:33 +0100

    Modified CLI code not to block existing & running features

--- php/Graphpish/Cli.php
+++ php/Graphpish/Cli.php
@@ -4,22 +4,24 @@ namespace Graphpish;
 abstract class Cli {
 	public static function run($argv){
 		if(count($argv)==2) {
+			
 			if(strrchr($argv[1],'.')==".xt") {
 				$p=new Trace\Parser();
 				$result=$p->parse($argv[1])->getArrays();
+				$r=new Graph\Render();
+				$r->rStart()->rNodes($result["nodes"])->rEdges($result["edges"])->rEnd();
+				
 			} elseif (strrchr($argv[1],'.')==".gsql") {
 				$p=new Sql\Client();
 				$result=$p->addSource($argv[1])->process()->getArrays();
+				var_dump($result);
+				
 			} else {
 				die("Unrecognized file extension. ");
 			}
 			
-			var_dump($result);
-			
-			$r=new Graph\Render();
-			//$r->rStart()->rNodes($result["nodes"])->rEdges($result["edges"])->rEnd();
 		} else {
-			echo 'Usage: php '.__FILE__.' trace.xt'.NL;
+			echo 'Usage: php '.__FILE__.' trace.xt|dbconfig.gsql'.NL;
 		}
 	}
 }
