dc9ceaea9488f0557bf9b3d9b6f620ab7618203e
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-08 00:19:15 +0100

    Simplyfied Sql's parser a lot. (1)

--- php/Graphpish/Sql/Parser.php
+++ php/Graphpish/Sql/Parser.php
@@ -9,31 +9,15 @@ class Parser {
 	}
 	function process($rawData) {
 		foreach($rawData as $rawSection=>$sectionData) {
-			 if (preg_match("/^(.*?):(.*?)-(.*?)((\/.*?)*)$/",$rawSection,$m)>0) {
-				if(count($m)==5) {
-					$this->options[$m[1]][$m[2]][$m[3]]=$sectionData;
-				} else {
-					$m2=explode('/',$m[4]);
-					$unit=&$this->options[$m[1]][$m[2]][$m[3]];
-					for($i=1;$i<count($m2);$i++) {
-						$unit=&$unit[$m2[$i]];
-					}
-					$unit=$sectionData;
+			preg_match("/^(.+?)((:(.+?))*)((-(.+?))*)((\/.+?)*)$/",$rawSection,$m);
+			$m2=array_merge(array($m[1]),explode(':',$m[2]),explode('-',$m[5]),explode('/',$m[8]));
+			$unit=&$this->options;
+			for($i=0;$i<count($m2);$i++) {
+				if($m2[$i]!='') {
+					$unit=&$unit[$m2[$i]];
 				}
-			} elseif (preg_match("/^node:(.*?)((\/.*?)*)$/",$rawSection,$m)>0) {
-				if(count($m)==3) {
-					$this->options["node"][$m[1]]=$sectionData;
-				} else {
-					$m2=explode('/',$m[2]);
-					$unit=&$this->options["node"][$m[1]];
-					for($i=1;$i<count($m2);$i++) {
-						$unit=&$unit[$m2[$i]];
-					}
-					$unit=$sectionData;
-				}
-			} else { // "connection" is expected here
-				$this->options[$rawSection]=$sectionData;
 			}
+			$unit=$sectionData;
 		}
 		return $this;
 	}
