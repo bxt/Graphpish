15a656d9121c308d0dd02c3c5b0648a52850b882
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-08 02:57:12 +0100

    More docs on ini-Parser

--- php/Graphpish/Sql/Parser.php
+++ php/Graphpish/Sql/Parser.php
@@ -1,10 +1,36 @@
 <?php
 namespace Graphpish\Sql;
 
+/**
+ * Parse multi-dimensional ini-Data from cascading files
+ * 
+ * @see \Graphpish\Sql\Parser::process()
+ */
 class Parser {
+	/**
+	 * Store current options
+	 */
 	private $options=array();
-	function __construct() {
-	}
+	/**
+	 * Parse ini-Data array into multi-level array
+	 * 
+	 * This method is made to make (well nearly) infinite
+	 * levels of configuration option stacking in .ini files
+	 * possible, by not only using sections "[foo]" but a set of
+	 * delimiters too: 
+	 * 
+	 * [foo:bar-baz/bam]
+	 * 
+	 * This section would create 4 dimensions. Notice that while
+	 * you can repeat the level delimiters, you can't change their
+	 * order currently.
+	 * 
+	 * Subsequent calls to this method on the same instance will
+	 * override older values. 
+	 *
+	 * @param array Like the ones returned by parse_ini_file()/string()
+	 * @return \Graphpish\Sql\Parser this (chainable)
+	 */
 	function process($rawData) {
 		foreach($rawData as $rawSection=>$sectionData) {
 			preg_match("/^(.+?)((:(.+?))*)((-(.+?))*)((\/.+?)*)$/",$rawSection,$m);
@@ -19,6 +45,11 @@ class Parser {
 		}
 		return $this;
 	}
+	/**
+	 * Access the parsed data
+	 *
+	 * @return array (assoc)
+	 */
 	function getOpts() {
 		return $this->options;
 	}
