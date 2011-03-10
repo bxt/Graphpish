f70c9cc1d26cc9ad2356963ab8e2567174fd8984
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-10 14:37:32 +0100

    Little fixes and doc on ArrayDecorator

--- php/Graphpish/Util/ArrayDecorator.php
+++ php/Graphpish/Util/ArrayDecorator.php
@@ -7,14 +7,28 @@ namespace Graphpish\Util;
  * Uses arrays like objects, passing them by reference
  */
 class ArrayDecorator {
-	public $array;
-	function __construct(&$array) {
+	/**
+	 * Holds the decorated array
+	 * @var array
+	 */
+	private $array;
+	
+	/**
+	 * Initialize with the decorated array
+	 */
+	public function __construct(array &$array) {
 		$this->array=&$array;
 	}
-	function &get() {
+	/**
+	 * Returns the underlaying array
+	 */
+	public function &get() {
 		return $this->array;
 	}
-	function store_deep($keys,$value) {
+	/**
+	 * Sotres a value into a deep path specified by keys
+	 */
+	public function store_deep($keys,$value) {
 		$keys=array_values($keys);
 		$unit=&$this->array;
 		for($i=0;$i<count($keys);$i++) {
@@ -23,7 +37,10 @@ class ArrayDecorator {
 		$unit=$value;
 		return $this;
 	}
-	function get_deep($keys) {
+	/**
+	 * Retrieves a value from a deep path specified by keys
+	 */
+	public function get_deep($keys) {
 		$keys=array_values($keys);
 		$unit=&$this->array;
 		for($i=0;$i<count($keys);$i++) {
@@ -31,12 +48,18 @@ class ArrayDecorator {
 		}
 		return $unit;
 	}
-	function flatten() {
+	/**
+	 * Works like array_values() but recursively
+	 */
+	public function flatten() {
 		$flat=new \stdClass;
 		$flat->a=array();
 		array_walk_recursive($this->array,"\\Graphpish\\Util\\ArrayDecorator::flatten_inner",$flat);
 		return $flat->a;
 	}
+	/**
+	 * Helper callback method for flatten()
+	 */
 	private static function flatten_inner($val,$key,$flat) {
 		$flat->a[]=$val;
 	}
