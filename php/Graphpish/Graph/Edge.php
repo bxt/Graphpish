99a648b0ae7cb19c4abddf6c686242b65e909fe9
Bernhard Häussner <bxt@die-optimisten.net>
2011-03-10 01:26:31 +0100

    Removed some legacy code

--- php/Graphpish/Graph/Edge.php
+++ php/Graphpish/Graph/Edge.php
@@ -11,10 +11,6 @@ class Edge extends Element {
 	public function __construct($from,$to) {
 		$this->from=$from;
 		$this->to=$to;
-		if(!isset(self::$byLabels[$from->getLabel()])) {
-			self::$byLabels[$from->getLabel()]=array();
-		}
-		self::$byLabels[$from->getLabel()][$to->getLabel()]=$this;
 	}
 	
 	/**
@@ -31,29 +27,4 @@ class Edge extends Element {
 		return $this->to;
 	}
 	
-	private static $byLabels=array();
-	
-	public static function getByLabels(HasLabelI $from,HasLabelI $to) {
-		if(
-				isset(self::$byLabels[$from->getLabel()]) &&
-				isset(self::$byLabels[$from->getLabel()][$to->getLabel()])
-				) {
-			return self::$byLabels[$from->getLabel()][$to->getLabel()];
-		}
-		return new self($from,$to);
-	}
-	public static function getAll() {
-		$array=array();
-		foreach(self::$byLabels as $x) {
-			foreach($x as $edge) {
-				$array[]=$edge;
-			}
-		}
-		return $array;
-	}
-	public static function aggregateLink($from,$to) {
-		$edge=self::getByLabels($from,$to);
-		$edge->increaseWeight();
-		return $edge;
-	}
 }
--- php/Graphpish/Graph/Node.php
+++ php/Graphpish/Graph/Node.php
@@ -9,7 +9,6 @@ class Node extends Element implements HasLabelI {
 	 */
 	public function __construct($label) {
 		$this->label=$label;
-		self::$byLabel[$label]=$this;
 	}
 	
 	/**
@@ -23,26 +22,5 @@ class Node extends Element implements HasLabelI {
 	 */
 	public function getId() {
 		return "node".substr(md5($this->label),0,8);
-	}
-	
-	private static $byLabel=array();
-	
-	public static function getByLabel($label) {
-		if(isset(self::$byLabel[$label])) {
-			return self::$byLabel[$label];
-		}
-		return new static($label);
-	}
-	public static function aggregateNode($label) {
-		$node=self::getByLabel($label);
-		$node->increaseWeight();
-		return $node;
-	}
-	public static function getAll() {
-		$array=array();
-		foreach(self::$byLabel as $node) {
-			$array[]=$node;
-		}
-		return $array;
 	}
 }
